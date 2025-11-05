20fff84c87d5e6b051eeb2614d9e227d6559dd51
Eleftherios Batzolis <egbatzo@cs.ihu.gr>
2025-11-05 23:51:37 +0200

    Added bin demos

---
+++ microwatt_caravel_user_project/bin_demos/donut.d
@@ -0,0 +1,62 @@
+donut.o: donut.c /usr/powerpc64le-linux-gnu/include/stdc-predef.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h \
+ /home/iamme/asic_tools/litex-boards/litex_boards/targets/build/digilent_nexys4/software/include/../libc/picolibc.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/float.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stddef.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h \
+ /usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stdarg.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdlib.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/stdlib.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/alloca.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/stdlib.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/ssp.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/strings.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/string.h \
+ /home/iamme/asic_tools/litex/litex/soc/software/libbase/console.h
+/usr/powerpc64le-linux-gnu/include/stdc-predef.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h:
+/home/iamme/asic_tools/litex-boards/litex_boards/targets/build/digilent_nexys4/software/include/../libc/picolibc.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+/usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/float.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+/usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stddef.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h:
+/usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stdarg.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdlib.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/stdlib.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/alloca.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/stdlib.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/ssp.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/strings.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ssp/string.h:
+/home/iamme/asic_tools/litex/litex/soc/software/libbase/console.h:
---
+++ microwatt_caravel_user_project/bin_demos/helloc.d
@@ -0,0 +1,38 @@
+helloc.o: helloc.c /usr/powerpc64le-linux-gnu/include/stdc-predef.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h \
+ /home/iamme/asic_tools/litex-boards/litex_boards/targets/build/digilent_nexys4/software/include/../libc/picolibc.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/float.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stddef.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h \
+ /usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stdarg.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h \
+ /home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h
+/usr/powerpc64le-linux-gnu/include/stdc-predef.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h:
+/home/iamme/asic_tools/litex-boards/litex_boards/targets/build/digilent_nexys4/software/include/../libc/picolibc.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+/usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/float.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+/usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stddef.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h:
+/usr/lib/gcc-cross/powerpc64le-linux-gnu/13/include/stdarg.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h:
+/home/iamme/asic_tools/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h:
